d4d644edca24c2502bffbcd067e154c1620308ac
csci185student <eli@unca.edu>
2023-03-13 12:12:03 -0400

    worked on lecture 15

--- lectures/lecture15/01-style-property-demo/main.js
+++ lectures/lecture15/01-style-property-demo/main.js
@@ -2,21 +2,32 @@
 function makeRed() {
     // your code here...
     console.log('Change background to red');
-    document.querySelector('body').style.backgroundColor = 'red';
+    document.querySelector('body').style.backgroundColor = '#E15554';
 };
 
 function makeBlue() {
     // your code here...
     console.log('Change background to blue');
+    document.querySelector('body').style.backgroundColor = '#98A6D4';
 };
 
 function makePink() {
     // your code here...
     console.log('Change background to pink');
+    document.querySelector('body').style.backgroundColor = '#FFC09F';
 };
 
 function makeOrange() {
     // your code here...
     console.log('Change background to orange');
+    document.querySelector('body').style.backgroundColor = '#F76C5E';
+    document.querySelector('#section1').style.backgroundColor = '#F79D65';
+    document.querySelector('.my-section').style.color = 'white';
 };
 
+function clearScreen() {
+    console.log('Clear all sections');
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.color = 'white';
+    document.querySelector('.my-section').style.border = 'white';
+}
