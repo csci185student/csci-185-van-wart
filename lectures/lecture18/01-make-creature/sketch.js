25c92a2b625a97674fed25cf2b2a3f93c454fbca
csci185student <eli@unca.edu>
2023-03-27 12:30:45 -0400

    lecture 18

--- lectures/lecture18/01-make-creature/sketch.js
+++ lectures/lecture18/01-make-creature/sketch.js
@@ -3,22 +3,34 @@ function setup() {
     const canvasHeight = window.innerHeight; 
     createCanvas(canvasWidth, canvasHeight);
 
-
-    
-
-    // your creature:
-    fill('yellow')
-    circle(300, 300, 300)
-
-    fill('black')
-    ellipse(250, 250, 30, 40)
-    ellipse(350, 250, 30, 40)
-
-
-
-
-
+    makeCreature(200,400,'yellow', 'green');
+    makeCreature(1000,500,'hotpink','gray');
+    makeCreature(600,400,'cyan', 'navy');
+    makeCreature(1000,100,'limegreen', 'blue');
+    makeCreature(1400,700, 'purple');
+    makeCreature(300,50, 'orange');
+    makeCreature(400,700, 'red', 'teal');
 
     // for debugging:
     drawGrid(canvasWidth, canvasHeight)
 }
+
+function makeCreature(x,y,fillColor, eyeColor='black') {
+    
+    // your creature:
+    fill(fillColor)
+    circle(x, y, 300)
+
+    fill(eyeColor)
+    ellipse(x - 50, y - 50, 30, 40)
+    ellipse(x + 50, y - 50, 30, 40)
+}
+
+function mouseClicked(){
+    makeCreature(mouseX,mouseY,'hotpink','gray')
+}
+
+function mouseDragged() {
+    makeCreature(mouseX,mouseY,'cyan', 'navy');
+}
+
