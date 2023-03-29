25278024f971749afe5320a6ae4c5dd19912d678
csci185student <eli@unca.edu>
2023-03-29 12:20:18 -0400

    lecture

--- lectures/lecture18/04-game-controller/sketch.js
+++ lectures/lecture18/04-game-controller/sketch.js
@@ -1,7 +1,8 @@
-let x = 100;
+let x = 200;
 let y = 200;
-let width = 50;
-let fillColor = 'white';
+let width = 150;
+let fillColor = 'hotpink';
+let paused = true;
 
 const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight; 
@@ -9,25 +10,53 @@ const canvasHeight = window.innerHeight;
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
 
-    // fill('red');
-    noFill();
+    fill(fillColor);
+    // noFill();
     circle(x, y, width);
 
     drawGrid(canvasWidth, canvasHeight);
 }
 
-const moveController = ev => {
+function moveController(ev) {
     console.log(ev.code);
-    // left arrow moves circle left
     // right arrow moves circle right
+    if (ev.code === 'ArrowRight' && !paused) {
+        console.log('move tie fighter right')
+        x = x + 10;
+    // left arrow moves circle left
+    } else if (ev.code === 'ArrowLeft') {
+            console.log('move tie fighter left')
+            x = x - 10;
     // up arrow moves circle up
+    } else if (ev.code === 'ArrowUp') {
+        console.log('move tie fighter up')
+        y = y - 10;
     // down arrow moves circle down
-
+    } else if (ev.code === 'ArrowDown') {
+        console.log('move tie fighter down')
+        y = y + 10;
+    // resize circle
+    } else if (ev.code === 'Space') {
+        console.log('make tie fighter bigger')
+        width = width + 10;
+    } else if (ev.code === 'Enter') {
+        console.log('make tie figther smaller')
+        width = width - 10;
+    // change color of circle
+    } else if (ev.code === 'KeyR') {
+        console.log('make tie fighter red');
+        fillColor = 'red';
+    } else if (ev.code === 'KeyB') {
+        console.log('make tie fighter blue');
+        fillColor = 'cyan';
+    }
     // redraw circle:
     clear();
-    noFill();
+    fill(fillColor);
     circle(x, y, width);
     drawGrid(canvasWidth, canvasHeight);
+
+    
 }
 
 
