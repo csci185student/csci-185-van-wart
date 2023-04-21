ebdfb9b6208a1629d7ebf0b55b42cf97a2504baf
csci185student <eli@unca.edu>
2023-04-21 11:08:52 -0400

    spotify

--- lectures/lecture15/01-style-property-demo/main.js
+++ lectures/lecture15/01-style-property-demo/main.js
@@ -2,27 +2,28 @@
 function makeRed() {
     // your code here...
     console.log('Change background to red');
-    document.querySelector('body').style.backgroundColor = '#E15554';
+    document.querySelector('#section1').style.backgroundColor = '#E15554';
 };
 
 function makeBlue() {
     // your code here...
     console.log('Change background to blue');
-    document.querySelector('body').style.backgroundColor = '#98A6D4';
+    document.querySelector('#section2').style.backgroundColor = '#98A6D4';
 };
 
 function makePink() {
     // your code here...
     console.log('Change background to pink');
-    document.querySelector('body').style.backgroundColor = '#FFC09F';
+    document.querySelector('#section3').style.backgroundColor = '#FFC09F';
 };
 
 function makeOrange() {
     // your code here...
     console.log('Change background to orange');
-    document.querySelector('body').style.backgroundColor = '#F76C5E';
-    document.querySelector('#section1').style.backgroundColor = '#F79D65';
-    document.querySelector('.my-section').style.color = 'white';
+    document.querySelector('#section4').style.backgroundColor = '#F76C5E';
+    // document.querySelector('#section1').style.backgroundColor = '#F79D65';
+    // document.querySelector('.my-section').style.color = 'white';
+    document.querySelector('#section4').style.fontSize = '50px';
 };
 
 function clearScreen() {
@@ -30,4 +31,16 @@ function clearScreen() {
     document.querySelector('body').style.backgroundColor = 'white';
     document.querySelector('body').style.color = 'white';
     document.querySelector('.my-section').style.border = 'white';
+}
+
+function multicolor() {
+    console.log('change all colors');
+    document.querySelector('#section1').style.backgroundColor = '#98A6D4';
+    document.querySelector('#section1').innerHTML = "Hello";
+    document.querySelector('#section2').style.backgroundColor = '#FFC09F';
+    document.querySelector('#section2').innerHTML = "there";
+    document.querySelector('#section3').style.backgroundColor = '#F76C5E';
+    document.querySelector('#section3').innerHTML = "friend";
+    document.querySelector('#section4').style.backgroundColor = '#E15554';
+    document.querySelector('#section4').innerHTML = "!";
 }
